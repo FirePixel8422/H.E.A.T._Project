5c2ec12569f8c91be4ced78d3519175cdf82ec5d
FirePixel8422 <bmtminecraft@hotmail.com>
2025-10-13 12:55:47 +0200

    Update EyeLookAtPlayer.cs

--- Assets/UI/NewUI/Scripts/Armory/EyeLookAtPlayer.cs
+++ Assets/UI/NewUI/Scripts/Armory/EyeLookAtPlayer.cs
@@ -2,12 +2,21 @@ using UnityEngine;
 
 public class EyeLookAtPlayer : MonoBehaviour
 {
-    public GameObject player;
-    private void Update()
+    [SerializeField] private Transform player;
+    [SerializeField] private float snappiness = 0.25f;
+
+
+
+    private void OnEnable() => UpdateScheduler.RegisterUpdate(OnUpdate);
+    private void OnDisable() => UpdateScheduler.UnRegisterUpdate(OnUpdate);
+
+
+    private void OnUpdate()
     {
-        if(player != null)
-        {
-            transform.LookAt(player.transform);
-        }
+        if (player == null) return;
+
+        Vector3 toPlayerDir = (player.transform.position - transform.position).normalized;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(Vector3.forward, toPlayerDir), snappiness * Time.deltaTime);
     }
 }
