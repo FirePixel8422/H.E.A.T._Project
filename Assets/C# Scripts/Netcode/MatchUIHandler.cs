2fbf658dee9efe009d64a561d249c3fc6e9f7d6a
FirePixel8422 <bmtminecraft@hotmail.com>
2025-10-29 11:16:24 +0100

    Attempt 3 for Match fix

--- Assets/C# Scripts/Netcode/MatchUIHandler.cs
+++ Assets/C# Scripts/Netcode/MatchUIHandler.cs
@@ -2,11 +2,15 @@ using FirePixel.Networking;
 using TMPro;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class MatchUIHandler : MonoBehaviour
 {
-    public static MatchUIHandler Instance {get; private set; }
+    public static MatchUIHandler Instance { get; private set; }
 
+    [SerializeField] private GameObject matchUI;
+    [SerializeField] string matchSceneName = "Luke Test Scene";
 
     [SerializeField] private TextMeshProUGUI[] scoreTextObj;
     private int[] roundPoints;
@@ -27,6 +31,16 @@ public class MatchUIHandler : MonoBehaviour
     private void OnEnable() => UpdateScheduler.RegisterUpdate(OnUpdate);
     private void OnDisable() => UpdateScheduler.UnRegisterUpdate(OnUpdate);
 
+    private void Start()
+    {
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        matchUI.SetActive(scene.name == matchSceneName);
+    }
+
     private void OnUpdate()
     {
         if (MatchManager.Instance.MatchState != MatchState.FightingPhase) return;
@@ -36,8 +50,9 @@ public class MatchUIHandler : MonoBehaviour
 
     public string IntToClockString(int time)
     {
-        string s = time.ToString("D4");
-        return $"{s[..2]}:{s.Substring(2, 2)}";
+        int minutes = time / 60;
+        int seconds = time % 60;
+        return $"{minutes:D2}:{seconds:D2}";
     }
 
     public void AddPoint(int clientGameId)
@@ -50,5 +65,11 @@ public class MatchUIHandler : MonoBehaviour
     public void OnMatchStart()
     {
         roundStartTime = NetworkManager.Singleton.ServerTime.TimeAsFloat;
+    }
+
+
+    private void OnDestroy()
+    {
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
