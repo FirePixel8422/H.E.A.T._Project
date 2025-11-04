bfa5e0b74d2dee20418eea23ca329680226e1421
R4HK00N <robert.eddy.beltman@gmail.com>
2025-11-04 15:49:32 +0100

    ingame menu script

--- Assets/UI/NewUI/Scripts/HUD/IngameMenuBehavior.cs
+++ Assets/UI/NewUI/Scripts/HUD/IngameMenuBehavior.cs
@@ -1,3 +1,6 @@
+using FirePixel.Networking;
+using System;
+using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
@@ -8,12 +11,17 @@ public class IngameMenuBehavior : MonoBehaviour
     public GameObject[] ingameMenuScreens;
     public GameObject globalVolume;
     private bool menuIsOpen;
+    public static Action OnMenuToggled { get; set; }
+
+
     public void OnOpenOrClose(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
         {
             if (!menuIsOpen)
             {
+                Cursor.lockState = CursorLockMode.None;
+
                 menuIsOpen = true;
 
                 ingameMenuScreens[0].SetActive(true);
@@ -24,8 +32,31 @@ public class IngameMenuBehavior : MonoBehaviour
                 menuIsOpen = false;
 
                 ingameMenuScreens[0].SetActive(false);
+                ingameMenuScreens[1].SetActive(false);
                 globalVolume.SetActive(false);
             }
+            OnMenuToggled?.Invoke();
         }
+    }
+    public void ResumeGame()
+    {
+        menuIsOpen = false;
+
+        ingameMenuScreens[0].SetActive(false);
+        globalVolume.SetActive(false);
+    }
+    public void IngameSettings()
+    {
+        ingameMenuScreens[0].SetActive(false);
+        ingameMenuScreens[1].SetActive(true);
+    }
+    public void Leavematch()
+    {
+        ClientManager.Instance.DisconnectClient_ServerRPC(ClientManager.LocalClientGameId);
+    }
+    public void GoBack()
+    {
+        ingameMenuScreens[1].SetActive(false);
+        ingameMenuScreens[0].SetActive(true);
     }
 }
