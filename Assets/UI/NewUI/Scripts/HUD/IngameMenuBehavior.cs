19e911c1ca8cac3fb5a9435a7df61094b49e1ff9
R4HK00N <robert.eddy.beltman@gmail.com>
2025-11-05 15:13:32 +0100

    lil fix

--- Assets/UI/NewUI/Scripts/HUD/IngameMenuBehavior.cs
+++ Assets/UI/NewUI/Scripts/HUD/IngameMenuBehavior.cs
@@ -2,36 +2,38 @@ using FirePixel.Networking;
 using System;
 using UnityEngine;
 
-
 public class IngameMenuBehavior : MonoBehaviour
 {
-    public GameObject[] ingameMenuScreens;
+    [Header("Menu Screens")]
+    public GameObject mainMenuScreen;
+    public GameObject settingsMenuScreen;
+
+    [Header("Other References")]
     public GameObject globalVolume;
+
     private bool menuIsOpen;
     public static Action<bool> OnMenuToggled { get; set; }
 
-
-
     private void OnEnable() => UpdateScheduler.RegisterUpdate(OnUpdate);
     private void OnDisable() => UpdateScheduler.UnRegisterUpdate(OnUpdate);
+
     private void OnUpdate()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            print("kanus");
             OnOpenOrClose();
         }
     }
-
 
     public void OnOpenOrClose()
     {
         if (!menuIsOpen)
         {
             Cursor.lockState = CursorLockMode.None;
-
             menuIsOpen = true;
 
-            ingameMenuScreens[0].SetActive(true);
+            mainMenuScreen.SetActive(true);
             globalVolume.SetActive(true);
         }
         else
@@ -39,31 +41,35 @@ public class IngameMenuBehavior : MonoBehaviour
             menuIsOpen = false;
             Cursor.lockState = CursorLockMode.Locked;
 
-            ingameMenuScreens[0].SetActive(false);
-            ingameMenuScreens[1].SetActive(false);
+            mainMenuScreen.SetActive(false);
+            settingsMenuScreen.SetActive(false);
             globalVolume.SetActive(false);
         }
+
         OnMenuToggled?.Invoke(menuIsOpen);
     }
+
     public void ResumeGame()
     {
         menuIsOpen = false;
-
-        ingameMenuScreens[0].SetActive(false);
+        mainMenuScreen.SetActive(false);
         globalVolume.SetActive(false);
     }
+
     public void IngameSettings()
     {
-        ingameMenuScreens[0].SetActive(false);
-        ingameMenuScreens[1].SetActive(true);
+        mainMenuScreen.SetActive(false);
+        settingsMenuScreen.SetActive(true);
     }
-    public void Leavematch()
+
+    public void LeaveMatch()
     {
         ClientManager.Instance.DisconnectClient_ServerRPC(ClientManager.LocalClientGameId);
     }
+
     public void GoBack()
     {
-        ingameMenuScreens[1].SetActive(false);
-        ingameMenuScreens[0].SetActive(true);
+        settingsMenuScreen.SetActive(false);
+        mainMenuScreen.SetActive(true);
     }
 }
